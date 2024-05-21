8ce7377e78b2d64b509b8c784446e3ec0b34b758
Ali <idris1390@gmail.com>
2024-05-21 16:58:20 +0200

    route ajouter et route fini

--- models/dcm.js
+++ models/dcm.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const dcmSchema = mongoose.Schema({
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
-    content:String,
-    subCategory: { type: mongoose.Schema.Types.ObjectId, ref: 'souscategory' },
-    origins: String,
-    target: String,
+    content:{ type:String, required: true},
+    subCategory: { type: mongoose.Schema.Types.ObjectId, ref: 'souscategory', required: true },
+    origins: {type :String ,required: true },
+    target:  {type :String ,required: true },
     likes:[{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
     dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
     date: Date,
-    type:Boolean,
+    type: {type: Boolean ,required: true },
 
 })
 const dcm = mongoose.model('dcm', dcmSchema);
