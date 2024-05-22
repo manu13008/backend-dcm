8c0c8164f703a3c981de18843fe838499b99a180
Manu <emmanuel.ayad@gmail.com>
2024-05-22 12:37:28 +0200

    ee

--- models/connection.js
+++ models/connection.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 CONNECTION_STRING = process.env.CONNECTION_STRING;
 console.log(process.env.CONNECTION_STRING);
 
-mongoose
-  .connect(CONNECTION_STRING, { connectTimeoutMS: 2000 })
+mongoose.connect(CONNECTION_STRING, { connectTimeoutMS: 2000 })
   .then(() => console.log("Database connected"))
   .catch((error) => console.error(error));
