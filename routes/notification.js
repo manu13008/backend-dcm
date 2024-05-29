11973e927d013da9ba915e5183031b3bd668ab51
Ali <idris1390@gmail.com>
2024-05-29 10:47:12 +0200

    ok

--- routes/notification.js
+++ routes/notification.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
-var express = require('express');
-var router = express.Router();
-const Notification = require('../models/notification');
-const { authenticateToken } = require('../modules/authentication')
+// const mongoose = require('mongoose');
+// var express = require('express');
+// var router = express.Router();
+// const Notification = require('../models/notification');
+// const { authenticateToken } = require('../modules/authentication')
 
 
 
-router.post('/notifications', authenticateToken, async (req, res) => {
-    try {
-      const notifications = await Notification.find({ userId: req.body.userId, isRead: false });
-      res.status(200).json(notifications);
-    } catch (error) {
-      res.status(500).json({ error: 'Server error' });
-    }
-  });
+// router.post('/notifications', authenticateToken, async (req, res) => {
+//     try {
+//       const notifications = await Notification.find({ userId: req.body.userId, isRead: false });
+//       res.status(200).json(notifications);
+//     } catch (error) {
+//       res.status(500).json({ error: 'Server error' });
+//     }
+//   });
 
-  module.exports = router;
+//   module.exports = router;
